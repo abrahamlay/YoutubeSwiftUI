4ecb765f91e718c9376d6652e8893740f29d614b
abraham.lay01 <abraham.lay01@homecredit.co.id>
2020-07-24 22:13:20 +0700

    Initial Commit

---
+++ YoutubeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YoutubeSwiftUI
+//
+//  Created by Abraham Lay (ID) on 24/07/20.
+//  Copyright © 2020 Abraham Lay (ID). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
