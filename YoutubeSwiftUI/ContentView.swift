b468fc13be27cb13ee341ae54ff5ac13712fe77b
abraham.lay01 <abraham.lay01@homecredit.co.id>
2020-07-24 23:20:01 +0700

    create navigation view and list

--- YoutubeSwiftUI/ContentView.swift
+++ YoutubeSwiftUI/ContentView.swift
@@ -10,12 +10,166 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home : View{
+    var body: some View{
+        NavigationView{
+                Content()
+                
+                .navigationBarItems(
+                    leading:
+                    HStack{
+                        Button(action: {print("Hello Button")}){
+                            Image("youtube").renderingMode(.original)
+                            .resizable()
+                                .frame(width:60, height: 60)
+                        }
+                    },
+                    trailing:
+                    HStack(spacing:20){
+                            Button(action: {print("Hello Print")}){
+                                Image(systemName: "tray.full").foregroundColor(Color.secondary)
+                            }
+                            
+                            Button(action: {print("Hello Print")}){
+                                Image(systemName: "video.fill").foregroundColor(Color.secondary)
+                            }
+                            
+                            Button(action: {print("Hello Print")}){
+                                Image(systemName: "magnifyingglass").foregroundColor(Color.secondary)
+                            }
+                            
+                            Button(action: {print("Hello Print")}){
+                                Image("profile")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .frame(width:20, height: 20)
+                                    .clipShape(Circle())
+                            }
+                    }
+                )
+            
+                    .navigationBarTitle("", displayMode: .inline)
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct Content : View {
+    var body: some View{
+        List{
+            //item 1
+            VStack{
+                ZStack(alignment: .bottomTrailing){
+                    Image("content1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                    Text("10:00")
+                        .padding(.all,5)
+                        .foregroundColor(Color.white)
+                        .font(.caption)
+                        .background(Color.black)
+                        .cornerRadius(5)
+                        .padding(.trailing,5)
+                        .padding(.bottom,5)
+                }
+                
+                HStack(spacing: 20){
+                    Image("profile")
+                        .resizable()
+                        .frame(width:30, height: 30)
+                        .clipShape(Circle())
+                    VStack (alignment: .leading){
+                        Text("Tutorial Beginner Android #1")
+                            .font(.headline)
+                        Text("Menjadi Android Developer itu mudah...")
+                            .font(.caption)
+                        Text("300x ditonton - 9 jam yang lalu")
+                            .font(.caption)
+                    }
+                    Spacer()
+                    Image(systemName: "list.bullet")
+                }
+                
+            }
+            //item 2
+            VStack{
+                ZStack(alignment: .bottomTrailing){
+                    Image("content2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                    Text("10:00")
+                        .padding(.all,5)
+                        .foregroundColor(Color.white)
+                        .font(.caption)
+                        .background(Color.black)
+                        .cornerRadius(5)
+                        .padding(.trailing,5)
+                        .padding(.bottom,5)
+                }
+                
+                HStack(spacing: 20){
+                    Image("profile")
+                        .resizable()
+                        .frame(width:30, height: 30)
+                        .clipShape(Circle())
+                    VStack (alignment: .leading){
+                        Text("Tutorial Beginner Android #2")
+                            .font(.headline)
+                        Text("Menjadi Android Developer itu mudah...")
+                            .font(.caption)
+                        Text("300x ditonton - 9 jam yang lalu")
+                            .font(.caption)
+                    }
+                    Spacer()
+                    Image(systemName: "list.bullet")
+                }
+                
+            }
+            
+            //item 3
+            
+            VStack{
+                ZStack(alignment: .bottomTrailing){
+                    Image("content3")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                    Text("10:00")
+                        .padding(.all,5)
+                        .foregroundColor(Color.white)
+                        .font(.caption)
+                        .background(Color.black)
+                        .cornerRadius(5)
+                        .padding(.trailing,5)
+                        .padding(.bottom,5)
+                }
+                
+                HStack(spacing: 20){
+                    Image("profile")
+                        .resizable()
+                        .frame(width:30, height: 30)
+                        .clipShape(Circle())
+                    VStack (alignment: .leading){
+                        Text("Tutorial Beginner Android #3")
+                            .font(.headline)
+                        Text("Menjadi Android Developer itu mudah...")
+                            .font(.caption)
+                        Text("300x ditonton - 9 jam yang lalu")
+                            .font(.caption)
+                    }
+                    Spacer()
+                    Image(systemName: "list.bullet")
+                }
+                
+            }
+        }
     }
 }
